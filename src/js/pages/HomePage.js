2fba30b4f5f63e12abaf73a54270c238c84fbbd0
Enoch Nyako <nyakoapps@gmail.com>
2019-08-28 23:37:39 +0000

    refactor class components to functional components with hooks

--- src/js/pages/HomePage.js
+++ src/js/pages/HomePage.js
@@ -1,50 +1,50 @@
-import React, { Component } from 'react';
-import '../../styles/App.css';
-import banner from '../../assets/landscape_one_unsplash.jpg';
-import {loadLatestPosts} from "../actions";
-import {connect} from "react-redux";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+
+import "../../styles/App.css";
+import { loadLatestPosts } from "../actions";
 import Post from "../components/Post";
 import MagazineHeader from "../components/MagazineHeader";
 import MagazineFooter from "../components/MagazineFooter";
+import banner from "../../assets/landscape_one_unsplash.jpg";
 
-class HomePage extends Component {
+function HomePage({ latestPosts, loadLatestPosts }) {
+  useEffect(() => {
+    loadLatestPosts();
+  }, []);
 
-    componentDidMount(){
-        this.props.loadLatestPosts();
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <MagazineHeader />
-                <div className="main-content">
-                    <div className="banner-image">
-                        <img src={banner} alt="banner"/>
-                        <p>Latest Posts</p>
-                    </div>
-                    <div className="container" style={{paddingBottom: "16px"}}>
-                        <div className="row" style={{marginTop: "4px"}}>
-                            {
-                                this.props.latestPosts.map((post) => {
-                                    return <div key={post.id}  className="col-sm-12 col-md-12 col-lg-4"><Post data={post} /></div>
-                                })
-                            }
-                        </div>
-                    </div>
+  return (
+    <div className="App">
+      <MagazineHeader />
+      <div className="main-content">
+        <div className="banner-image">
+          <img src={banner} alt="banner" />
+          <p>Latest Posts</p>
+        </div>
+        <div className="container" style={{ paddingBottom: "16px" }}>
+          <div className="row" style={{ marginTop: "4px" }}>
+            {latestPosts.map(post => {
+              return (
+                <div key={post.id} className="col-sm-12 col-md-12 col-lg-4">
+                  <Post data={post} />
                 </div>
-                <MagazineFooter />
-            </div>
-        );
-    }
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <MagazineFooter />
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
-    latestPosts: state.posts.latestPosts
+  latestPosts: state.posts.latestPosts
 });
 
 export default connect(
-    mapStateToProps,
-    {
-        loadLatestPosts
-    }
-)(HomePage)
+  mapStateToProps,
+  {
+    loadLatestPosts
+  }
+)(HomePage);
--- src/js/pages/PostDetailsPage.js
+++ src/js/pages/PostDetailsPage.js
@@ -1,71 +1,124 @@
-import React, { Component } from 'react';
-import { Helmet } from 'react-helmet';
-import '../../styles/App.css';
-import {connect} from "react-redux";
-import {loadPostDetails} from "../actions";
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+import "../../styles/App.css";
+import { connect } from "react-redux";
+import { loadPostDetails } from "../actions";
 import MagazineHeader from "../components/MagazineHeader";
 import MagazineFooter from "../components/MagazineFooter";
 
-class PostDetailsPage extends Component {
+function PostDetailsPage({
+  loadPostDetails,
+  postDetails,
+  match: {
+    params: { slug }
+  }
+}) {
+  useEffect(() => {
+    loadPostDetails(slug);
+  }, []);
 
-    componentWillMount(){
-        this.props.loadPostDetails(this.props.match.params["slug"]);
-    }
+  return (
+    <div className="App">
+      <MagazineHeader />
+      {!postDetails ? null : (
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{postDetails.seo_meta.title}</title>
+          <meta name="description" content={postDetails.seo_meta.metadesc} />
+          <link rel="canonical" href={postDetails.seo_meta.canonical} />
+          <meta
+            name="keywords"
+            content={`${postDetails.seo_meta.focuskw}, ${postDetails.seo_meta.metakeywords}`}
+          />
+          <meta
+            name="og:site_name"
+            property="og:site_name"
+            content={postDetails.seo_meta.opengraph_site_name}
+          />
+          <meta
+            name="og:locale"
+            property="og:locale"
+            content={postDetails.seo_meta.opengraph_locale}
+          />
+          <meta
+            name="og:type"
+            property="og:type"
+            content={postDetails.seo_meta.opengraph_type}
+          />
+          <meta
+            name="og:title"
+            property="og:title"
+            content={postDetails.seo_meta.opengraph_title}
+          />
+          <meta
+            name="og:description"
+            property="og:description"
+            content={postDetails.seo_meta.opengraph_description}
+          />
+          <meta
+            name="og:url"
+            property="og:url"
+            content={postDetails.seo_meta.opengraph_url}
+          />
+          <meta
+            name="og:image"
+            property="og:image"
+            content={postDetails.seo_meta.opengraph_image}
+          />
+          <meta
+            name="twitter:title"
+            content={postDetails.seo_meta.twitter_title}
+          />
+          <meta
+            name="twitter:description"
+            content={postDetails.seo_meta.twitter_description}
+          />
+          <meta
+            name="twitter:image"
+            content={postDetails.seo_meta.twitter_image}
+          />
+        </Helmet>
+      )}
+      <div className="main-content">
+        {!postDetails ? null : (
+          <div className="banner-image">
+            <img
+              className="img img-fluid"
+              src={postDetails.thumbnail_url}
+              alt={postDetails.thumbnail.alt}
+            />
+          </div>
+        )}
 
-    render() {
-        return (
-            <div className="App">
-                <MagazineHeader />
-              { !this.props.postDetails ? null : 
-                    <Helmet>
-                        <meta charSet="utf-8" />
-                        <title>{this.props.postDetails.seo_meta.title}</title>
-                        <meta name="description" content={this.props.postDetails.seo_meta.metadesc} />
-                        <link rel="canonical" href={this.props.postDetails.seo_meta.canonical} />
-                        <meta name="keywords" content={`${this.props.postDetails.seo_meta.focuskw}, ${this.props.postDetails.seo_meta.metakeywords}`} />
-                        <meta name="og:site_name" property="og:site_name" content={this.props.postDetails.seo_meta.opengraph_site_name} />
-                        <meta name="og:locale" property="og:locale" content={this.props.postDetails.seo_meta.opengraph_locale} />
-                        <meta name="og:type" property="og:type" content={this.props.postDetails.seo_meta.opengraph_type} />
-                        <meta name="og:title" property="og:title" content={this.props.postDetails.seo_meta.opengraph_title} />
-                        <meta name="og:description" property="og:description" content={this.props.postDetails.seo_meta.opengraph_description} />
-                        <meta name="og:url" property="og:url" content={this.props.postDetails.seo_meta.opengraph_url} />
-                        <meta name="og:image" property="og:image" content={this.props.postDetails.seo_meta.opengraph_image} />
-                        <meta name="twitter:title" content={this.props.postDetails.seo_meta.twitter_title} />
-                        <meta name="twitter:description" content={this.props.postDetails.seo_meta.twitter_description} />
-                        <meta name="twitter:image" content={this.props.postDetails.seo_meta.twitter_image} />
-
-                    </Helmet>
-             }
-                <div className="main-content">
-                    {!this.props.postDetails ? null :
-                        <div className="banner-image">
-                            <img className="img img-fluid" src={this.props.postDetails.thumbnail_url} alt={this.props.postDetails.thumbnail.alt}/>
-                        </div>
-                    }
-
-                    {!this.props.postDetails ? null :
-                        <div className="container">
-                            <div className="row" style={{padding: "8px"}}>
-                                <div className="col-lg-8 col-sm-12 col-md-12" style={{minHeight: "600px", padding: "8px"}}>
-                                    <h1 className="post-title">
-                                        {this.props.postDetails.title}
-                                    </h1>
-                                    <div className="post-content" dangerouslySetInnerHTML={{__html: this.props.postDetails.content[0].text}} />
-                                </div>
-
-                            </div>
-                        </div>
-                    }
-                </div>
-                <MagazineFooter />
+        {!postDetails ? null : (
+          <div className="container">
+            <div className="row" style={{ padding: "8px" }}>
+              <div
+                className="col-lg-8 col-sm-12 col-md-12"
+                style={{ minHeight: "600px", padding: "8px" }}
+              >
+                <h1 className="post-title">{postDetails.title}</h1>
+                <div
+                  className="post-content"
+                  dangerouslySetInnerHTML={{
+                    __html: postDetails.content[0].text
+                  }}
+                />
+              </div>
             </div>
-        );
-    }
+          </div>
+        )}
+      </div>
+      <MagazineFooter />
+    </div>
+  );
 }
 
-
 const mapStateToProps = state => ({
-    postDetails: state.posts.postDetails
+  postDetails: state.posts.postDetails
 });
 
-export default connect(mapStateToProps, {loadPostDetails})(PostDetailsPage)
+export default connect(
+  mapStateToProps,
+  { loadPostDetails }
+)(PostDetailsPage);
